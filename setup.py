c5c18b2d96401821954366edf6b7c2b5c12e6a32
Divij Bindlish <dvjbndlsh93@gmail.com>
2015-09-01 20:08:41 +0530

    Fix minor syntax error

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
 
 setup(
     name='parse-torrent-name',
-    version=__import__('ptn').__version__,
-    author=__import__('ptn').__author__,
-    author_email=__import__('ptn').__email__,
-    license=__import__('ptn').__license__,
+    version=__import__('PTN').__version__,
+    author=__import__('PTN').__author__,
+    author_email=__import__('PTN').__email__,
+    license=__import__('PTN').__license__,
     url='https://github.com/divijbindlish/parse-torrent-name',
     description='Parse torrent name of a movie or TV show',
     long_description=description,
     packages=['PTN'],
     keywords=('parse parser torrent torrents name names proper rename '
               'movie movies tv show shows series extract find quality '
-              'group codec audio resolution title season episode year ')
+              'group codec audio resolution title season episode year '),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
