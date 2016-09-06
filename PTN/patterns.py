f3621b7461d93540425b8832df760084eb63a23b
Roi Dayan <roi.dayan@gmail.com>
2016-09-06 08:46:05 +0300

    Fix HDTS is quality and not audio
    DTS is audio and HDTS is telesync.
    
    Signed-off-by: Roi Dayan <roi.dayan@gmail.com>

--- PTN/patterns.py
+++ PTN/patterns.py
@@ -6,12 +6,12 @@ patterns = [
     ('episode', '([ex]([0-9]{2})(?:[^0-9]|$))'),
     ('year', '([\[\(]?((?:19[0-9]|20[01])[0-9])[\]\)]?)'),
     ('resolution', '([0-9]{3,4}p)'),
-    ('quality', ('((?:PPV\.)?[HP]DTV|(?:HD)?CAM|B[DR]Rip|TS|(?:PPV '
-                 ')?WEB-?DL(?: DVDRip)?|HDRip|DVDRip|DVDR'
-                 'IP|CamRip|W[EB]BRip|BluRay|DvDScr|hdtv)')),
+    ('quality', ('((?:PPV\.)?[HP]DTV|(?:HD)?CAM|B[DR]Rip|(?:HD-?)?TS|'
+                 '(?:PPV )?WEB-?DL(?: DVDRip)?|HDRip|DVDRip|DVDRIP|'
+                 'CamRip|W[EB]BRip|BluRay|DvDScr|hdtv)')),
     ('codec', '(xvid|[hx]\.?26[45])'),
-    ('audio', ('(MP3|DD5\.?1|Dual[\- ]Audio|LiNE|H?DTS|AAC(?:\.?2\.0)'
-               '?|AC3(?:\.5\.1)?)')),
+    ('audio', ('(MP3|DD5\.?1|Dual[\- ]Audio|LiNE|DTS|AAC(?:\.?2\.0)?|'
+               'AC3(?:\.5\.1)?)')),
     ('group', '(- ?([^-]+(?:-={[^-]+-?$)?))$'),
     ('region', 'R[0-9]'),
     ('extended', '(EXTENDED(:?.CUT)?)'),
